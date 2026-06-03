7145dd0fb66f2a4fc6682879c477781ad95e606f
adheebabdulla007 <adheebabdulla10@gmail.com>
2026-06-03 06:06:34 +0530

    added BaseEntity with Id, CreatedAt, UpdatedAt

---
+++ src/Pursuit.Domain/Entities/BaseEnitity.cs
@@ -0,0 +1,8 @@
+﻿namespace Pursuit.Domain.Entities;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
