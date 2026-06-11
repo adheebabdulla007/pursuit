be8041a8feb8c2f758542db65819cf543c48fae1
adheebabdulla007 <adheebabdulla10@gmail.com>
2026-06-11 07:45:27 +0530

    fix(api): add JsonStringEnumConverter to support string enum deserialization

--- src/Pursuit.API/Program.cs
+++ src/Pursuit.API/Program.cs
@@ -8,7 +8,12 @@ using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(
+            new System.Text.Json.Serialization.JsonStringEnumConverter());
+    });
 builder.Services.AddOpenApi();
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
